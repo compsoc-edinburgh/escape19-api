f5af1bbb8f50fabaad16e30fd2d9ad652f7873ba
Qais Patankar <qaisjp@gmail.com>
2018-02-27 13:46:12 +0000

    Add missing list content

---
+++ pkg/api/list/get.go
@@ -0,0 +1,75 @@
+package list
+
+import (
+	"bytes"
+	"encoding/csv"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	stripe "github.com/stripe/stripe-go"
+)
+
+func (i *Impl) Get(c *gin.Context) {
+	if c.Query("pw") != i.Config.StatsPass {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"status":  "error",
+			"message": "Unauthorised.",
+		})
+		return
+	}
+
+	params := &stripe.OrderListParams{}
+	params.Expand("data.charge.balance_transaction")
+	params.Filters.AddFilter("status", "", "paid")
+
+	// day before orders went out, utc timestamp
+	params.Filters.AddFilter("created", "gt", "1518714907")
+
+	buf := new(bytes.Buffer)
+	writer := csv.NewWriter(buf)
+
+	writer.Write([]string{
+		"order_id",
+		"owner_name", "owner_email",
+		"uun",
+		"meal_starter", "meal_main", "meal_dessert",
+		"purchaser_name", "purchaser_email",
+		"over18",
+		"auth_token",
+		"charge_net",
+		"charge_fees",
+	})
+
+	orders := i.Stripe.Orders.List(params)
+	for orders.Next() {
+		o := orders.Order()
+
+		hasSKU := false
+		for _, item := range o.Items {
+			if item.Parent == i.Config.Stripe.SKU {
+				hasSKU = true
+			}
+		}
+
+		if !hasSKU {
+			continue
+		}
+
+		writer.Write([]string{
+			o.ID,
+			o.Meta["owner_name"], o.Meta["owner_email"],
+			o.Meta["uun"],
+			o.Meta["meal_starter"], o.Meta["meal_main"], o.Meta["meal_dessert"],
+			o.Meta["purchaser_name"], o.Meta["purchaser_email"],
+			o.Meta["over18"],
+			o.Meta["auth_token"],
+			strconv.FormatInt(o.Charge.Tx.Net, 10),
+			strconv.FormatInt(o.Charge.Tx.Fee, 10),
+		})
+	}
+
+	writer.Flush()
+
+	c.String(http.StatusOK, buf.String())
+}
