cdc461712c4988ffc179c7365d6c8ab7e0bb718b
Qais Patankar <qaisjp@gmail.com>
2018-03-04 22:08:31 +0000

    disable stats

--- pkg/api/stats/get.go
+++ pkg/api/stats/get.go
@@ -1,19 +1,17 @@
 package stats
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func (i *Impl) Get(c *gin.Context) {
-	sku, err := i.Stripe.Skus.Get(i.Config.Stripe.SKU, nil)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"status":  "error",
-			"message": "Unknown error.",
-		})
-	}
+	// sku, err := i.Stripe.Skus.Get(i.Config.Stripe.SKU, nil)
+	// if err != nil {
+	// 	c.JSON(http.StatusInternalServerError, gin.H{
+	// 		"status":  "error",
+	// 		"message": "Unknown error.",
+	// 	})
+	// }
 
-	c.JSON(http.StatusOK, struct{ Quantity int64 }{sku.Inventory.Quantity})
+	// c.JSON(http.StatusOK, struct{ Quantity int64 }{sku.Inventory.Quantity})
 }
